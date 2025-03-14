509f0678a8a79bea2c17c402f31f387d681003ea
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-14 11:15:54 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -2,5 +2,5 @@ import sys
 
 x, y, w, h = map(int, sys.stdin.readline().split())
 
-# x와 w의 차, y와 h의 차, x와 0의 차, y와 0의 차 중 적은 수
+# x와 w의 차, y와 h의 차, x와 0의 차, y와 0의 차 중 작은 수
 print(min(abs(x-w), abs(y-h), abs(x-0), abs(y-0)))
