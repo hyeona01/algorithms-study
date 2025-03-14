def6568ba1587adc6217eb14ba8143f2fc929953
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-14 14:06:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,11 @@
+import sys
+
+print(ord(sys.stdin.readline().strip()))
+
+# input = sys.stdin.readline()
+
+# if type(input) == type(1):
+#   print(ord(int(input)))
+
+# else:
+#   print(ord(str(input)))
