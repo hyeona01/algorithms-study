67863b70696ef684c45cac0e9f73daaf74b753a7
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-20 01:17:55 +0900

    2468번 안전 영역 복습

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,46 +1,47 @@
+# 복습
+
 import sys
 sys.setrecursionlimit(10000)
 
-N = int(sys.stdin.readline())
+# 안전영역
+# 잠길 수 있는 영역은 0부터 가장 높은 높이 -1
+# DFS를 사용해서 인접한 지역을 재귀적으로 탐색 -> 탐색이 종료되면 return 1
+# 전체 행, 열을 순회하여 return 받을 때마다 count 를 올려줌
+
+n = int(sys.stdin.readline())
 area = []
-
-max_height = 0 # 입력 지역 중 가장 높은 지역의 높이
-
-for i in range(N):
-  lst = list(map(int, sys.stdin.readline().split()))
-  area.append(lst)
-  max_height = max(max(lst), max_height)
+max_height = 0
+max_safe_cnt = 0
 
 dx = [0, 0, -1, 1] # 상 하 좌 우
 dy = [-1, 1, 0, 0] # 상 하 좌 우
 
-# 잠기는 높이 / 행 / 열 / 방문한 원소의 배열(T or F)
-def check_border(height, r, c, visited):
-  # 상하좌우 DFS
+for i in range(n):
+  lst = list(map(int, sys.stdin.readline().split()))
+  area.append(lst)
+  max_height = max(max_height, max(lst))
+
+# 해당 높이보다 높은 좌표는 안전 영역으로 표시하는 함수
+def dfs(height, r, c, visited):
+  # 인접한 상하좌우 깊이 탐색(DFS)
   for i in range(4):
-    nr = r + dx[i] # 다음 행
-    nc = c + dy[i] # 다음 열
-    
-    if 0 <= nr < N and 0 <= nc < N: # 다음 원소가 유효한 범위 내에 있는지 확인
-        if not visited[nr][nc] and area[nr][nc] > height:
-            visited[nr][nc] = True
-            check_border(height, nr, nc, visited) # 재귀적으로 DFS 탐색
+    nx = r+dx[i]
+    ny = c+dy[i]
+    if 0 <= nx < n and 0 <= ny < n: # 유효한 경로인지 검증
+      if not visited[nx][ny] and area[nx][ny] > height:
+        visited[nx][ny] = True
+        dfs(height, nx, ny, visited)
 
-# 메인 함수 - 0부터 가장 높은 높이까지 안전 영역 연산
-def safty_area():
-    global max_height
-    max_safty_area = 0
+# 침수 높이를 0부터 max-1까지 순회하며 탐색
+for h in range(0, max_height):
+  visited = [[False] * n for _ in range(n)] # 잠기지 않는 지역을 표시하는 2차원 배열
+  count = 0
+  for i in range(n): # 행
+    for j in range(n): # 열
+      if not visited[i][j] and area[i][j] > h: # 아직 방문 전이라면
+        visited[i][j] = True # 방문하고
+        dfs(h, i, j, visited) # 인접 지역 방문
+        count += 1
+  max_safe_cnt = max(max_safe_cnt, count)
 
-    for height in range(max_height):  # 높이 0부터 max_height까지
-        visited = [[False] * N for _ in range(N)] # 초기화
-        count = 0 # 초기화
-
-        for i in range(N): # 행
-            for j in range(N): # 열
-                if not visited[i][j] and area[i][j] > height: # 방문하지 않거나 안전 영역일 경우
-                    check_border(height, i, j, visited)
-                    count += 1
-        max_safty_area = max(max_safty_area, count)
-    return max_safty_area
-
-print(safty_area())
+print(max_safe_cnt)
