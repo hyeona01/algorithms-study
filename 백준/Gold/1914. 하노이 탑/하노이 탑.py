7d1909039b6c315fd1ef85e614a9caa7ba40ce6b
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-19 21:35:03 +0900

    [Gold V] Title: 하노이 탑, Time: 820 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,12 +1,21 @@
-def f(n, a, b, c):
-  if(n == 1):
-    print(a, c, sep = " ")
-  else:
-    f(n-1, a, c, b) #1단계
-    f(1, a, b, c) #2단계
-    f(n-1, b, a, c) #3단계
+# 복습
 
-n = int(input()) #입력
-print(2**n-1) #이동횟수 출력
-if(n <= 20): #함수 조건
-  f(n, 1, 2, 3) #함수 호출
+import sys
+
+n = int(sys.stdin.readline())
+
+def hanoi(n, start, temp, end):
+  # 변수 정의
+  # 1. 원판의 출발지, 원판의 도착지, 원판의 경유지
+  # 2. 원판의 개수
+  if n == 1:
+    # print(f'{n}원판이 {start}에서 {end}로 이동')
+    print(start, end)
+    return
+  hanoi(n-1, start, end, temp)
+  hanoi(1, start, temp, end)
+  hanoi(n-1, temp, start, end)
+
+print(2**n - 1)
+if n <= 20:
+  hanoi(n, 1, 2, 3)
