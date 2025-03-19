04910dad0dca62fa1c6433778bac30001c05f6ed
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-19 23:17:29 +0900

    1071번 Z 문제 복습

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,39 +1,50 @@
+# 복습
+
 import sys
 
-N, r, c = map(int, sys.stdin.readline().split())
+n, r, c = map(int, sys.stdin.readline().split())
+result = 0
 
-# key: 찾고자 하는 행(r)과 열(c)을 주어진 배열 크기의 4등분을 하며 찾는다.
+# 백트래킹+재귀로 풀이
 
-# (1) 2의 N-1승 * 2의 N-1승 으로 4등분을 한다.
-# (2) 어느 사분면에 찾고자 하는 r, c가 속하는 지 찾는다.
-# (3) 해당 사분면의 시작점만큼 count를 증가시킨다.
-# (4) (2) ~ (4)를 반복하고, 
-#      N == 1가 된다면 r, c와 일치하는 분면에 맞게 count를 증가시켜준다.
-
-def recursive_z(n, x, y, count):
-  # (4) 종료 조건 설정
+def z(n, row, col, order):
+  global result
   if n == 1:
-    if r == x and c == y:
-      return count
-    elif r == x and c > y:
-      return count + 1
-    elif r > x and c == y:
-      return count + 2
+    if r == row and c == col:
+      result = order
+      return
+    elif r == row and c > col:
+      result = order + 1
+      return
+    elif r > row and c == col:
+      result = order + 2
+      return
     else:
-      return count + 3
+      result = order + 3
+      return
   
-  # (1) 4등분한다.
-  section = 2 ** (n-1)
+  # n = 3 일 때를 가정함. 
+  # 간격: 2^(n-1)
+  # 하나의 사분면 사이즈: 2^(n-1) * 2^(n-1)
+  # 0 <= row <= 3 and 0 <= col <= 3 -> 0~15
+  # 0 <= row <= 3 and 4 <= col <= 7 -> 16~31
+  # 4 <= row <= 7 and 0 <= col <= 3 -> 32~47
+  # 4 <= row <= 7 and 4 <= col <= 7 -> 48~63
 
-  # (2, 3) 어느 사분면에 속하는지 판단하고 해당 사분면의 시작점만큼 count를 설정한다.
-  if r < x+section and c < y+section: # 제1사분면
-    return recursive_z(n-1, x, y, count)
-  elif r < x+section and c >= y+section: # 제2사분면
-    return recursive_z(n-1, x, y+section, count + section*section)
-  elif r >= x+section and c < y+section: # 제3사분면
-    return recursive_z(n-1, x+section, y, count + section*section*2)
-  elif r >= x+section and c >= y+section: # 제4사분면
-    return recursive_z(n-1, x+section, y+section, count + section*section*3)
+  size = 2 ** (n-1) * 2**(n-1)
+  one_side = 2**(n-1)
 
-# x, y의 시작점은 (0,0)으로 세팅하며 count를 초기화한다.
-print(recursive_z(N, 0, 0, 0))
+  if row <= r < row+one_side and col <= c < col + one_side:
+    z(n-1, row, col, order + 0) # 1사분면
+
+  elif row <= r < row + one_side and col + one_side <= c < col + 2*one_side:
+    z(n-1, row, col + one_side, order + size) # 2사분면
+
+  elif row+one_side <= r < row + 2*one_side and col <= c < col + one_side:
+    z(n-1, row + one_side, col, order + size * 2) # 3사분면
+
+  elif row+one_side <= r < row + 2*one_side and col + one_side <= c < col + 2*one_side:
+    z(n-1, row + one_side, col + one_side, order + size * 3) # 4사분면
+
+z(n, 0, 0, 0)
+print(result)
