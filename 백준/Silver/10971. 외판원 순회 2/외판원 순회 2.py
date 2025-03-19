d7e64a3e781ef8eb3d3f33496a4051facc133d01
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-20 00:27:54 +0900

    외판원 순회 2 복습

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,44 +1,42 @@
+# 복습
+
 import sys
 
-N = int(sys.stdin.readline())
-W = []
-for i in range(N):
-  W.append(list(map(int, sys.stdin.readline().split())))
+# 외판원 순회 2 - TSP
+# n개의 도시를 중복 없이 순회하고 다시 출발 도시로 돌아온다.
+# 행렬로 i에서 j로 이동하는 비용을 주어준다.
+# 가장 비용이 적은 루트의 비용을 출력한다.
 
-visited = [False] * N
-visited[0] = True
-travel_city = [0] # 방문한 도시의 인덱스가 순서대로 담긴다.
-result = float('inf')
+n = int(sys.stdin.readline())
+w = []
+visited = [False] * n
+visited[0] = True # 0번 도시를 출발 도시로 정한다.
+route = [0] # 도시의 인덱스를 저장
+min_expense = float('inf')
 
-# goal: 도시의 순서를 적절하게 재배치한다.
-def travel(city):
-  # global N
-  # global W
-  # global visited
-  # global travel_city
-  global result
+for i in range(n):
+  w.append(list(map(int, sys.stdin.readline().split())))
 
-  if city == N:
-    # 출발지로 돌아가는 경로가 존재하는지 체크
-    if W[travel_city[-1]][0] != 0:
-      # 최적 경로의 비용을 계산한다.
-      temp = 0
+def tsp(city):
+  global min_expense
 
-      for i in range(N-1):
-        temp += W[travel_city[i]][travel_city[i+1]]
-      # 출발지로 돌아가는 비용을 추가한다.
-      temp += W[travel_city[-1]][0]
-      result = min(result, temp)
-    return
+  if city == n:
+    if w[route[-1]][0] != 0:
+      expense = 0
+      for i in range(n-1):
+        expense += w[route[i]][route[i+1]]
+      expense += w[route[-1]][0] # 0번 도시로 돌아가는 비용 추가
+      min_expense = min(min_expense, expense)
+      return
   
-  for i in range(1, N): # 0번 도시는 이미 방문하였기에 1번 도시부터 위치를 찾기
-    # 이전 노드에서 i 노드로 이동할 수 있는지 확인
-    if not visited[i] and W[travel_city[-1]][i] != 0: # 아직 방문하지 않았다면 방문하고 비용 계산하기
-      travel_city.append(i)
-      visited[i] = True
-      travel(city + 1)
-      visited[i] = False
-      travel_city.pop()
+  for i in range(1, n): # 0번 도시로 시작하는 것은 고정되었기 때문
+    if visited[i] == False:
+      if w[route[-1]][i] != 0:
+        visited[i] = True
+        route.append(i)
+        tsp(city + 1)
+        visited[i] = False
+        route.pop()
 
-travel(1)
-print(result)
+tsp(1)
+print(min_expense)
