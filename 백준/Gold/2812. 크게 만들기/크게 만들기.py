4397b8733f5e15277e028b6de48aad6ef32f3867
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-24 17:58:17 +0900

    [Gold III] Title: 크게 만들기, Time: 300 ms, Memory: 39036 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+N, K = map(int, sys.stdin.readline().strip().split())
+nums = deque(sys.stdin.readline().strip())
+
+stack = []
+
+cnt = 0
+for num in nums:
+  # if not stack: # 비어있다면
+  #   stack.append(num)
+
+  while stack and stack[-1] < num and cnt < K:
+    cnt += 1
+    stack.pop()
+
+  stack.append(num)
+
+  # else:
+  #   stack.append(num)
+
+# 987654 의 경우 k만큼 제거하지 못함
+while cnt < K:
+  stack.pop()
+  cnt += 1
+
+for num in stack:
+  print(num, end='')
