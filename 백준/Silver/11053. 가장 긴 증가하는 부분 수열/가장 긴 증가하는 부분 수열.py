627f476efa866cf251f067b4f62a1ea6b76fc5e5
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-26 16:24:47 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,19 +1,40 @@
 import sys
-from itertools import combinations
 
-N = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 
-# 가장 긴 증가하는 부분 수열
-dp = [1] * N # 하나의 원소는 최소 수열의 길이가 1
+def find_idx(lst, x):
+  start = 0
+  end = len(lst) - 1
+  while start <= end:
+    mid = (start + end) // 2
 
-for i in range(N):
-  max_dp = 0
-  for j in range(i): # dp[i]를 구하기 위해 0~i까지의 원소와 해당 dp값 확인
-    if arr[j] < arr[i] and dp[j] > max_dp: # 조건에 해당하는 원소 중 가장 큰 원소의 인덱스(dp[j])에서 1 더한 수를 dp[i]로 업데이트
-      max_dp = max(dp[j], max_dp)
-      dp[i] = dp[j] + 1
+    if x == lst[mid]:
+      return mid
+    elif x < lst[mid]:
+      end = mid - 1
+    elif x > lst[mid]:
+      start = mid + 1
+  return start
 
-print(max(dp))
-# print(arr)
-# print(dp)
+lis = []
+if n == 1:
+  lis.append(arr[0])
+if n == 2:
+  if arr[1] > arr[0]:
+    lis.append(arr[0])
+    lis.append(arr[1])
+  else:
+    lis.append(arr[0])
+
+elif n > 2:
+  for i in range(n):
+    if not lis:
+      lis.append(arr[i])
+    elif lis[-1] < arr[i]:
+      lis.append(arr[i])
+    else:
+      lis[find_idx(lis, arr[i])] = arr[i]
+
+# print(lis)
+print(len(lis))
