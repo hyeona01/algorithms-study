f4b1280baa038d20c8f2cca5a27e75fb611e337f
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-03-29 01:48:55 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 244 ms, Memory: 51144 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,35 +1,9 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-# 경로 압축과 합병시 트리 높이를 기준으로 합치기
-def find(parents, rank, x):
-  if parents[x] != x:
-    parents[x] = find(parents, rank, parents[x])  # 경로 압축
-  return parents[x]
-
-
-def union(parents, rank, a, b):
-  rootA = find(parents, rank, a)
-  rootB = find(parents, rank, b)
-
-  if rootA == rootB:  # 사이클 발견
-    return False
-
-  # 트리 높이를 기준으로 합침
-  if rank[rootA] > rank[rootB]:
-    parents[rootB] = rootA
-  elif rank[rootA] < rank[rootB]:
-    parents[rootA] = rootB
-  else:
-    parents[rootB] = rootA
-    rank[rootA] += 1  # 두 트리의 높이가 같으면 한쪽의 높이를 증가시킴
-  return True
-
-
 V, E = map(int, sys.stdin.readline().split())
 edges = []
 parents = [i for i in range(V + 1)]
-rank = [0] * (V + 1)  # 트리의 높이를 저장하는 배열
 
 for i in range(E):
   a, b, c = map(int, sys.stdin.readline().split())
@@ -38,13 +12,28 @@ for i in range(E):
 edges.sort(key=lambda x: x[2]) # 가중치를 기준으로 오름차순 정렬
 
 mst_cost = 0
-mst_cnt = 0
+
+def find(x):
+  if parents[x] == x:
+    return x
+  parents[x] = find(parents[x])
+  return parents[x] # 부모 노드를 따라 재귀적으로 루트 노드 찾기
+
+def union(a , b):
+  rootA = find(a)
+  rootB = find(b)
+
+  if rootA == rootB: # 사이클 발견
+    return False
+  
+  if rootA < rootB:
+    parents[rootB] = rootA
+  else:
+    parents[rootA] = rootB
+  return  True # 사이클 없음
 
 for a, b, c in edges:
-  if union(parents, rank, a, b): # 사이클 없을 때
+  if union(a, b): # 사이클 없을 때
     mst_cost += c
-    mst_cnt += 1
-    if V - 1 == mst_cnt: # MST 완성(간선은 V-1개)
-      break
 
 print(mst_cost)
