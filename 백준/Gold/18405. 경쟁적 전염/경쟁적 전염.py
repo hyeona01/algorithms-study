80ff361884783b1f56d2cb777c785c4812cf3bca
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-04-03 11:30:53 +0900

    [Gold V] Title: 경쟁적 전염, Time: 100 ms, Memory: 35032 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -18,23 +18,25 @@ S, X, Y = map(int, sys.stdin.readline().strip().split())
 dx = [-1, 1, 0, 0] #상하좌우
 dy = [0, 0, -1, 1]
 
-# 바이러스 번호 낮은 순서로
-virus_list.sort()
-queue = deque(virus_list)
+def BFS():
+  # 바이러스 번호 낮은 순서로
+  virus_list.sort()
+  queue = deque(virus_list)
 
-while queue:
-  virus, time, x, y = queue.popleft()
-  # S초
-  if time == S:
-    break
+  while queue:
+    virus, time, x, y = queue.popleft()
+    # S초
+    if time == S:
+      break
 
-  for i in range(4):
-    nx, ny = x + dx[i], y + dy[i]
-    
-    if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == 0:
-      arr[nx][ny] = virus  # 바이러스 전염
-      queue.append((virus, time + 1, nx, ny))  # 시간 +1
-
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      
+      if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == 0:
+        arr[nx][ny] = virus  # 바이러스 전염
+        queue.append((virus, time + 1, nx, ny))  # 시간 +1
+BFS()
 print(arr[X-1][Y-1])
 
 # # 특정 좌표에서 인접한 곳에 바이러스를 전염
