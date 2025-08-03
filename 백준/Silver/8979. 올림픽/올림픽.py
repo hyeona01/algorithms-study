b09e259bdac0c919501e896d6c4a0ee250128942
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2025-08-03 20:21:35 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,30 +2,33 @@ import sys
 
 n, k = map(int, sys.stdin.readline().split())
 
-gold_list = [0] * n
-silver_list = [0] * n
-bronze_list = [0] * n
+gold_list = [0] * (n+1)
+silver_list = [0] * (n+1)
+bronze_list = [0] * (n+1)
 
 # 입력 받기
-for i in range(n):
+for _ in range(n):
   num, gold, silver, bronze = map(int, sys.stdin.readline().split())
-  gold_list[i] = gold
-  silver_list[i] = silver
-  bronze_list[i] = bronze
+  gold_list[num] = gold
+  silver_list[num] = silver
+  bronze_list[num] = bronze
 
 result = 1
 # 순위 결정
-for i in range(n):
+for i in range(1, n+1):
+  if i == k:
+    continue
+
   if gold_list[i] > gold_list[k]:
     result += 1
-    break
+    continue
   elif gold_list[i] == gold_list[k]:
     if silver_list[i] > silver_list[k]:
       result += 1
-      break
+      continue
     elif silver_list[i] == silver_list[k]:
       if bronze_list[i] > bronze_list[k]:
         result += 1
-        break
+        continue
 
 print(result)
