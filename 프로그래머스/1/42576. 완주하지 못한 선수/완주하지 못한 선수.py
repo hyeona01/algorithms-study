18cf1a2cbae45611d07b83962c5b1f804433add1
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2026-03-26 15:52:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.74 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,14 @@
+from collections import Counter
+
 def solution(participant, completion):
-    sum_hash = 0
-    hash_dict = {}    
+    # Counter로 풀이
+    part_counter = Counter(participant)
+    comp_counter = Counter(completion)
+    # print(part_counter)
     
-    # 참여자 해시 생성 및 해시 총합 구하기
-    for player in participant:
-        hash_key = hash(player)
-        hash_dict[hash_key] = player
-        sum_hash += hash_key
+    # 빼기 연산
+    answer = part_counter - comp_counter
+    answer = list(answer.keys())
+    # print(answer)
     
-    # 완주자 해시를 총합에서 빼기
-    for player in completion:
-        sum_hash -= hash(player)
-    
-    # 남은 총합의 해시 value return
-    return hash_dict[sum_hash]
-    
-    # return answer
+    return answer[0]
