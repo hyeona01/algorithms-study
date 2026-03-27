493814295cafed0a40e18ab8db60398850076909
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2026-03-27 13:59:13 +0900

    [level 2] Title: 타겟 넘버, Time: 371.82 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,24 +1,20 @@
-cnt = 0
-
-def dfs(t, numbers, idx, sum_nums):
-    global cnt
-    
-    # 종료 조건: numbers 길이만큼 연산했을 때 return
-    # print("idx",idx)
-    # print("sum_nums, target",sum_nums, t)
-    # print("cnt",cnt)
-    if idx == len(numbers):
-        if sum_nums == t:
-            cnt += 1
-        return cnt
-
-    # 수행 방법: 더하기, 빼기의 경우를 재귀적으로 연산
-    dfs(t, numbers, idx + 1, sum_nums + numbers[idx])
-    dfs(t, numbers, idx + 1, sum_nums - numbers[idx])
-    
 def solution(numbers, target):
-    global cnt
+    cnt = 0
     
-    dfs(target, numbers, 0, 0)
+    # DFS 구현체
+    def dfs(idx, sum_nums):
+        nonlocal cnt
+        
+        # 종료 조건: numbers 길이만큼 연산했을 때 return
+        if idx == len(numbers):
+            if sum_nums == target:
+                cnt += 1
+            return cnt
+
+        # 수행 방법: 더하기, 빼기의 경우를 재귀적으로 연산
+        dfs(idx + 1, sum_nums + numbers[idx])
+        dfs(idx + 1, sum_nums - numbers[idx])
+    
+    dfs(0, 0)
     
     return cnt
