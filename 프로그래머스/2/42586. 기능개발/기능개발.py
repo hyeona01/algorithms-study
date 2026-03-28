982d9ed9953b4da39564f80add36b98587ddb160
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2026-03-28 17:01:25 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -5,24 +5,24 @@ def solution(progresses, speeds):
     # 며칠 후 작업이 완료되는 지 계산한 queue 생성
     # pop하며 현재 숫자 이하인 원소는 전부 pop
     
-    done = deque()
+    days = deque()
     for i in range(len(progresses)):
         need_prog = 100 - progresses[i]
-        done.append(math.ceil(need_prog / speeds[i]))
+        days.append(math.ceil(need_prog / speeds[i]))
     
     deployment = []
-    max_date = done.popleft()
+    release_day = days.popleft()
     feature = 1
-    while len(done) > 0:
-        curr = done.popleft()
+    while len(days) > 0:
+        curr = days.popleft()
         # 이전 작업에 포함하여 배포 가능
-        if max_date >= curr:
+        if release_day >= curr:
             feature += 1
         # 이후 일정에 배포
         else:
             deployment.append(feature)
             feature = 1
-            max_date = curr
+            release_day = curr
     # 마지막 일정 배포
     deployment.append(feature)
     
